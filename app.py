09bd53a9c01cce6983aa0cdfad3018baf78ae1e8
AK391 <ahsen@huggingface.co>
2024-12-31 19:04:51 -0500

    update

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+os.system("pip install ai-gradio[deepseek,crewai,hyperbolic,openai,gemini,smolagents]==0.2.1")
 from app_lumaai import demo as demo_lumaai
 from app_allenai import demo as demo_allenai
 from app_claude import demo as demo_claude
