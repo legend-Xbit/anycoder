f318731f0bc74e5696d4a38553b7b6ec630c9469
AK391 <ahsen@huggingface.co>
2025-01-10 15:48:51 -0500

    remove moondream add phi-4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from app_moondream import demo as demo_moondream
+from app_transformers import demo as demo_transformers
 from app_lumaai import demo as demo_lumaai
 from app_allenai import demo as demo_allenai
 from app_claude import demo as demo_claude
@@ -37,7 +37,7 @@ from utils import get_app
 
 # Create mapping of providers to their demos
 PROVIDERS = {
-    "MoonDream 2.0": demo_moondream,
+    "Phi-4": demo_transformers,
     "OpenAI Coder": demo_openai_coder,
     "Gemini Coder": demo_gemini_coder,
     "OpenAI": demo_openai,
@@ -74,7 +74,7 @@ PROVIDERS = {
     "NVIDIA": demo_nvidia,
 }
 
-demo = get_app(models=list(PROVIDERS.keys()), default_model="MoonDream 2.0", src=PROVIDERS, dropdown_label="Select Provider")
+demo = get_app(models=list(PROVIDERS.keys()), default_model="Phi-4", src=PROVIDERS, dropdown_label="Select Provider")
 
 if __name__ == "__main__":
     demo.queue(api_open=False).launch(show_api=False)
