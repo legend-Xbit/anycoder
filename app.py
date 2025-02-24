2e0e49645890a251e462cc29ae0a3a08fbba2802
AK391 <ahsen@huggingface.co>
2025-02-24 17:06:20 -0500

    update code example

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ gr.load(
 "OpenRouter Coder": """import gradio as gr
     import ai_gradio
 gr.load(
-    name='openrouter:openai/o3-mini-2025-01-31',
+    name='openrouter:anthropic/claude-3.7-sonnet',
     src=ai_gradio.registry,
     coder=True
 ).launch()""",
