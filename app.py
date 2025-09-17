0f9a1b42ac35bb7bb93816c544ecfa33a2efb489
AK391 <ahsen@huggingface.co>
2025-09-16 23:22:48 -0400

    update code langauge list

--- app.py
+++ app.py
@@ -7158,7 +7158,7 @@ with gr.Blocks(
         )
         # Language dropdown for code generation (add Streamlit and Gradio as first-class options)
         language_choices = [
-            "gradio", "transformers.js", "html", "streamlit", "python", "svelte", "c", "cpp", "markdown", "latex", "json", "css", "javascript", "jinja2", "typescript", "yaml", "dockerfile", "shell", "r", "sql", "sql-msSQL", "sql-mySQL", "sql-mariaDB", "sql-sqlite", "sql-cassandra", "sql-plSQL", "sql-hive", "sql-pgSQL", "sql-gql", "sql-gpSQL", "sql-sparkSQL", "sql-esper"
+            "html", "gradio", "transformers.js", "streamlit", "python", "svelte", "c", "cpp", "markdown", "latex", "json", "css", "javascript", "jinja2", "typescript", "yaml", "dockerfile", "shell", "r", "sql", "sql-msSQL", "sql-mySQL", "sql-mariaDB", "sql-sqlite", "sql-cassandra", "sql-plSQL", "sql-hive", "sql-pgSQL", "sql-gql", "sql-gpSQL", "sql-sparkSQL", "sql-esper"
         ]
         language_dropdown = gr.Dropdown(
             choices=language_choices,
